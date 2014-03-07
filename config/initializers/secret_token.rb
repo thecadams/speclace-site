4dac5de0dcbda625b69ba1e5541109aafb947d18
Chris Adams <chris@cadams.com.au>
2014-03-07 11:34:28 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Speclace::Application.config.secret_key_base = 'e84fae9641271af6c4b9454682b9e19366b0634365109849a8414f2d957629454e4f91cf437bd951af371e449a57f218c03ff4d2c834ddf2dbdef2982364da25'
