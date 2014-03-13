f3f08eab164541c0e74889841b5cd2650c138211
Chris Adams <chris@cadams.com.au>
2014-03-13 10:46:26 -0700

    Product detail page
    - Todo: styling, zoom, tabs, small/medium/large images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order(:priority, created_at: :desc).all
   end
+
+  def show
+    @product = Product.where(name: params[:slug].titleize).first!
+  end
 end
