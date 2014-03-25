11555382407a9f9b6599fab5c5b3826f8ddb3912
Chris Adams <chris@cadams.com.au>
2014-03-25 01:22:16 -0700

    Store images to filesystem when running tests
    - Avoid S3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ Speclace::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.paperclip_defaults = {
+    storage: :filesystem
+  }
 end
